6ca96c8923fd4c63834891781bb2352535fbc3c9
Anmol-STRS <anmoldhiman444@gmail.com>
2024-05-23 10:19:56 -0400

    Added Docstring

--- projectreadmegenerator/generator.py
+++ projectreadmegenerator/generator.py
@@ -6,35 +6,72 @@ import os
 from dotenv import load_dotenv
 
 class generate:
+    
     def generatereadmefile():
+        
+        """
+        Takes the Storage class and the customuserinput class
+         
+        """
         STORAGE = Storage
         CUSTOMER_INPUT = custominput
 
+        """
+        Load Dotenv
+          
+        """
         load_dotenv(dotenv_path=STORAGE.env_module_loader())
         
+        
+        """
+        Get the API Key and the Model the user is using from the environment file and use it as it is 
+        
+        """
         API_KEY = os.getenv("API_KEY")
         MODEL_USING = os.getenv("MODEL")
-
+        
         if not API_KEY or not MODEL_USING:
+            """
+            If key or model is not found, the function ask for the key and the model and store
+            it in an environment variable  
+            
+            """
             print('Value not provided')
             API_KEY = API_KEY or CUSTOMER_INPUT.askfortheapikey().strip().replace(' ', '')
             MODEL_USING = MODEL_USING or CUSTOMER_INPUT.askforthemodel().strip().replace(' ', '')
             if(STORAGE.storetheapikey(API_KEY, MODEL_USING)): 
                 print('ENV file is added')
-            
-            
+        
+        """
+        Ask for the Project link from the user
+        """    
         PROJECT_LINK = CUSTOMER_INPUT.askforprojectlinkinput()
-
+        
+        
+        """
+        Configure the Gemini AI with the user API_KEY
+        
+        """
         genai.configure(api_key=API_KEY)
 
+        """
+        Inputs the model that user wants to use in GenAI
+        
+        """
         try:
             model = genai.GenerativeModel(model_name=MODEL_USING)
         except Exception as e:
             raise ValueError(f"Failed to initialize the model with name '{MODEL_USING}': {e}")
 
+        """
+        Generates a response 
+        """
         response = model.generate_content(f"Please generate a beautiful and step by step README FILE for the project link {PROJECT_LINK}")
 
         def writereadme():
+            """
+            Function generates Readme file based on the AI response
+            """
             while True:
                 try:
                     with open('README.md', 'w', encoding='utf-8') as readme_file:
