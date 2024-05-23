eaa5a938e58f5e33a9518f036ad15315308b2a2d
Anmol-STRS <anmoldhiman444@gmail.com>
2024-05-22 23:58:32 -0400

    Added the init information

--- projectreadmegenerator/__init__.py
+++ projectreadmegenerator/__init__.py
@@ -2,4 +2,8 @@ from .generator import generate
 from .customuserinput import custominput
 from .storage import Storage
 
+"""
+Generates the basic init file so that user can access the modules seperately 
+"""
+
 __all__ = ['generate', 'custominput', 'Storage']
